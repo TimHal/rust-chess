25a5f9d94376255235f8359e91ad5edc821be7e1
Tim Hallyburton <tim.hallyburton@protonmail.com>
2022-02-16 14:38:09 +0100

    Add module for Game and Context

---
+++ src/game.rs
@@ -0,0 +1,20 @@
+
+use core::*;
+
+/// This module contains the (standard) chess game logic with support for contexts
+mod game {
+
+    pub struct Game<'a> {
+        context: Option(Context),
+        board: Board
+    }
+
+    pub struct Context<'a> {
+        
+    }
+
+}
+
+fn main() {
+    println!("Hello from game");
+}
