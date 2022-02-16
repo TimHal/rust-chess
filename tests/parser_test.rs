af0465286f724a659ed8214b7489a44766d10073
Tim Hallyburton <tim.hallyburton@protonmail.com>
2022-02-16 23:23:56 +0100

    remove unused test

--- tests/parser_test.rs
+++ tests/parser_test.rs
@@ -4,19 +4,12 @@ use rust_chess::parser::parser::parse_fen;
 mod tests {
 
     #[test]
-    fn test_works() {
-        assert_eq!(1, 1);
-        let some = "Hello, World!";
-       
-    }
-
-
-    #[test]
     fn create_std_board_from_fen() {
         let std_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
         let board_in_starting_position = rust_chess::parser::parser::parse_fen(std_fen);
         assert_eq!(1, 1);
     }
+
 
         
 }
