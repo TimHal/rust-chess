97dfb0addcb7812f6c64aa3efb6467bccd023c49
TimHal <tim.hallyburton@gmx.de>
2022-02-19 12:24:07 +0100

    parser::parser changed to parser

--- tests/parser_test.rs
+++ tests/parser_test.rs
@@ -6,7 +6,7 @@ mod tests {
     #[test]
     fn create_std_board_from_fen() {
         let std_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-        let board_in_starting_position = rust_chess::parser::parser::parse_fen(std_fen);
+        let board_in_starting_position = rust_chess::parser::parse_fen(std_fen);
         assert_eq!(1, 1);
     }
 
