616d40bb5aea4dbd16c1b29bbb9848ff89e0cced
TimHal <tim.hallyburton@gmx.de>
2022-02-20 11:34:23 +0100

    Add more complicated fen for testing

--- tests/parser_test.rs
+++ tests/parser_test.rs
@@ -8,7 +8,15 @@ mod tests {
         let std_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
         let board_in_starting_position = rust_chess::parser::parse_fen(std_fen);
         assert_eq!(1, 1);
-    }
+   }
+
+   #[test]
+   fn create_test_position_from_fen() {
+    let testfen = "r4k1r/1p1n1ppp/2p2n2/p7/P2qP3/2NP1Q2/1PP2RPP/R5K1 w - - 0 1";
+    let board_in_test_position = rust_chess::parser::parse_fen(testfen);
+    // https://lichess.org/editor/r4k1r/1p1n1ppp/2p2n2/p7/P2qP3/2NP1Q2/1PP2RPP/R5K1_w_-_-_0_1
+    assert_eq!(1, 1);
+   }
 
 
         
