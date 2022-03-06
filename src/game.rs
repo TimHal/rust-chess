a7c5e9ca0cc816e77aeaf6cdd05b29408fbc5683
TimHal <tim.hallyburton@gmx.de>
2022-03-06 16:40:16 +0100

    Preparing basic game logic + states and tests

--- src/game.rs
+++ src/game.rs
@@ -1,33 +1,109 @@
+use crate::core::{Board, Piece, Color, Color::*, Square, Figure::*};
 
-use core::*;
+use std::collections::hash_set::HashSet;
+use itertools::Itertools;
 
-/// This module contains the (standard) chess game logic with support for contexts
-mod game {
+
     // Where to put the game state???
     // castling_privs: set of (Piece, Piece)  (King + castling rook)
     // e_p_pawns: set of pawns
 
-    pub struct Game<'a> {
-        context: Option(Context),
-        board: Board,
-        moves: Vec<Move>,
+pub struct Game {
+    meta: Option<GameMeta>,
+    board: Board,
+    state: State,
+    moves: Vec<Move>,
+}
+
+pub struct State {
+    // en-passant, castling etc is all done via the game, not the pieces or board as it requires knowledge about game state
+    turn: Color, // the color to make the next move
+    result: Option<GameResult>,
+    ckeck: Option<&Piece>, // can or can not be a king in check
+    castling_privileges: HashSet<(&Piece, &Piece)>, 
+    possible_en_passant_moves: HashSet<(&Piece, &Square)>, // the pawn to take en-passant and the target square
+}
+
+pub enum GameResult { WhiteWin, BlackWin, Draw }
+
+pub struct GameMeta {
+    // player info, year, place, tournament, player ratings etc etc
+}
+
+pub struct GameTreeNode {
+    
+}
+
+pub struct Move {
+    piece: Piece,
+    target_square: Square,
+    variations: Vec<Variation>
+}
+
+pub struct MoveMeta {
+    // move time, comments, engine evaluation etc
+}
+
+pub struct Variation {
+    
+}
+
+impl Game {
+
+    pub fn new() -> Self {
+
+        let mut board = Board::new();
+        let mut state = State::new(&board);
+        let mut meta = GameMeta::new();
+        let mut moves: Vec<Move> = vec! [];
+
+        Game {board: board, state: state, meta: Some(meta), moves: moves}
     }
 
-    pub struct Context<'a> {
+    pub fn make_move(&self, move_: &Move) -> Result<&Self> {
+        Ok(self)
     }
 
-    pub struct GameTreeNode {
+    pub fn get_moves(&self, color: Color) -> HashSet<Move> {
         
+        let mut result = HashSet::new();
+        // get available moves for all pieces
+        
+
+        // extend with castling moves
+
+        // extend with en-passant moves
+
+        // extend with promotion moves
     }
 
-    pub struct Move {
-        piece: Piece;
-        target_square: Square;
-        variations: Vector<Variation>;
+}
+
+impl State {
+
+    pub fn new(board: &Board) -> Self {
+        
+        let mut possible_en_passant_moves: HashSet<(&Piece, &Square)> = HashSet::new();
+        let mut castling_privileges: HashSet<(&Piece, &Piece)> = board.pieces.iter()
+                .filter(|p| p.figure == King || p.figure == Rook) // get the kings and rooks
+                .tuple_combinations()
+                .filter(|comb| (comb.0.figure == King || comb.1.figure == King) && (comb.0.color == comb.1.color))
+                .collect();
+
+        State { turn: White, 
+            result: None, 
+            check: None, 
+            castling_privileges: castling_privileges, 
+            possible_en_passant_moves: possible_en_passant_moves }
     }
 
-    pub struct Variation {
+}
+
+impl GameMeta {
+
+    pub fn new() -> Self {
 
     }
 
 }
+
---
+++ tests/game_test.rs
@@ -0,0 +1,46 @@
+use rust_chess::game::*;
+
+#[test]
+fn create_game() {
+    let game = Game::new();
+}
+
+#[test]
+fn available_moves() {
+    assert_eq!(1,1)
+}
+
+#[test]
+fn make_valid_moves() {
+    assert_eq!(1,1)
+}
+
+#[test]
+fn attempt_invalid_moves() {
+    assert_eq!(1,1)
+}
+
+#[test]
+fn is_checkmate() {
+    assert_eq!(1,1)
+}
+
+#[test]
+fn check_threefold_repitition() {
+    assert_eq!(1,1)
+}
+
+#[test]
+fn check_insufficient_material() {
+    assert_eq!(1,1)
+}
+
+#[test]
+fn check_en_passant_move() {
+    assert_eq!(1,1)
+}
+
+#[test]
+fn check_fifty_move_rule() {
+    assert_eq!(1,1)
+}
